9be82c41427e3fa2d8ba509d07724df554f20f05
Quentin PLAUD <qplaud@gmail.com>
2021-01-28 23:00:41 +0100

    app done

---
+++ README.md
@@ -0,0 +1,11 @@
+# Single Page Application - Progame App
+
+## Installation
+
+`git clone git@github.com:luca-montaigut/The_Hyper_Progame.git`
+
+`cd The_Hyper_Progame`
+
+`xdg-open index.html`
+
+`npm run build`
---
+++ src/javascripts/components.js
@@ -0,0 +1,28 @@
+const header = () => {
+  return `
+  <nav>
+    <br>
+    <div class="row">
+      <div class="col-lg-8">
+        <a href="#"><h1>PROGAME</h1></a>
+      </div>
+      <div class="col-lg-4">
+        <div id="searchbar" class="d-flex justify-content-between flew-row align-items-center form-div">
+          <i class="fas fa-search mx-3"></i>
+          <input type="text" id="findgame" value="" placeholder="Find a game..." class="form-control"><br><br>
+        </div>
+      </div>
+    <br>
+  </nav>
+  `;
+};
+
+const footer = () => {
+  return `
+  <footer class="mt-3">
+    <p>Kents Brockman © 2020</p>
+  </footer>
+  `;
+};
+
+export { header, footer };
